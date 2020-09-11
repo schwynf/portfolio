ae3f4e9a791c47e341680d56c2a2449d07d8a673
Schwyn Francis <schwynf@gmail.com>
2020-09-10 21:12:44 -0700

    trying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const express = require("express");
 const app = express();
+const path = require('path')
 // const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser")
 
