1e40dc875893767954690f95575f14044f606cc8
Schwyn Francis <schwynf@gmail.com>
2021-02-10 19:08:06 -0700

    checking logs

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
