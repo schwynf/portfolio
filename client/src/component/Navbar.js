34f487463bd5707bab9c7787bd6ab0b64299d37b
Schwyn Francis <schwynf@gmail.com>
2021-02-10 18:12:00 -0700

    navbar

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./Navbar.css"
 
 const Navbar = () => {
+
+
+
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark nav-bar">
+                <h1 onClick={()=>{
+                    window.location.href="/"
+                }} style={{fontFamily:"cursive", color:"#ffc107"}}>SF</h1>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
