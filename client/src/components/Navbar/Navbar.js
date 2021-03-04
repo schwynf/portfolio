d0488819035b82cb8d9b58a854430c43f50e0d79
Schwyn Francis <schwynf@gmail.com>
2021-03-03 18:36:12 -0700

    added svg logo, Ai

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css"
 import { Link } from 'react-router-dom';
+import { ReactSVG } from 'react-svg'
+import logo from "../../images/logo2.svg"
 
 const Navbar = () => {
 
@@ -9,7 +11,10 @@ const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark nav-bar" id="top">
-                <Link to="/">< h4 style={{ fontFamily: "Lucida Console, Courier New, monospace", color: "rgb(8, 19, 78)", fontSize: "2rem", backgroundColor: "black" }}>SF</h4></Link>
+                <div id="logo" style={{ width: "75px", height: "75px" }}>
+                    <ReactSVG src={logo} />
+                </div>
+                {/* <Link to="/">< h4 style={{ fontFamily: "Lucida Console, Courier New, monospace", color: "rgb(8, 19, 78)", fontSize: "2rem", backgroundColor: "black" }}>SF</h4></Link> */}
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
